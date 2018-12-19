00a5c5f3c32e89d8154b09f3913a7542f1f57feb
Liran Baruch <liran6@gmail.com>
2018-12-19 22:36:26 +0200

    18/12

--- main.cpp
+++ main.cpp
@@ -50,7 +50,7 @@ int main() {
     list1.push_back(i2);
     Expression* i3 = x.expressionEvaluate(" 1 / 3 / 1 / 4 / 1 / 5");
     list1.push_back(i3);
-    Expression* i4 = x.expressionEvaluate(" 1 / 3 * 1 / 4 * 1 / 5 ");
+    Expression* i4 = x.expressionEvaluate(" (1 / 3)* (1 / 4) * (1 / 5) ");
     list1.push_back(i4);
 
 
@@ -78,6 +78,9 @@ int main() {
  * -12
  * 0.1
  * 3
+ * 0.333333
+ * 0.166667
+ * 0.166667
  * */
 
     return 0;
